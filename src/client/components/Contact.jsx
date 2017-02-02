96b64f116e9d31859f7426faf3ae82d120a35f0e
Sherman <shc410@gmail.com>
2017-02-02 03:13:30 -0800

    remove unneccsary periods

--- src/client/components/Contact.jsx
+++ src/client/components/Contact.jsx
@@ -13,8 +13,8 @@ const Contact = () => {
         <h3><i className="fa fa-phone icon"></i>Phone</h3>
         <a className='link' href="tel:4156234670">415-623-4670</a>
         <h2>Some other stuff to look at</h2>
-        <p>Check out my <a className='link' href="https://www.linkedin.com/in/shermanchen410" target="_blank">LinkedIn</a>.</p>
-        <p>Check out my <a className='link' href='https://www.github.com/Sherman-Chen' target='_blank'>GitHub</a>.</p>
+        <p>Check out my <a className='link' href="https://www.linkedin.com/in/shermanchen410" target="_blank">LinkedIn</a></p>
+        <p>Check out my <a className='link' href='https://www.github.com/Sherman-Chen' target='_blank'>GitHub</a></p>
       </section>
       <Footer />
     </div>
