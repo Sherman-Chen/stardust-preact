1f766703843af5498f461f4cfa3999c210859afb
Sherman <shc410@gmail.com>
2017-02-02 02:58:36 -0800

    still tweaking h's

--- src/client/components/Contact.jsx
+++ src/client/components/Contact.jsx
@@ -8,9 +8,9 @@ const Contact = () => {
       <Header />
       <section className='stretcher'>
         <h1>Let's get in touch!</h1>
-        <h2><i className="fa fa-envelope icon"></i>Email</h2>
+        <h3><i className="fa fa-envelope icon"></i>Email</h3>
         <a className='link' href="mailto:shc410@gmail.com">shc410@gmail.com</a>
-        <h2><i className="fa fa-phone icon"></i>Phone</h2>
+        <h3><i className="fa fa-phone icon"></i>Phone</h3>
         <a className='link' href="tel:4156234670">415-623-4670</a>
         <h2>Some other stuff to look at</h2>
         <p>Check out my <a className='link' href="https://www.linkedin.com/in/shermanchen410" target="_blank">LinkedIn</a>.</p>
