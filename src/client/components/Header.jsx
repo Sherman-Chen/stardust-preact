4fff69fad9bfcf4787565f23b8608779c39d8ff6
Sherman <shc410@gmail.com>
2017-02-04 02:49:14 -0800

    wrap header a tags in p tags

--- src/client/components/Header.jsx
+++ src/client/components/Header.jsx
@@ -8,10 +8,10 @@ const Header = () => {
         <p><a href="/" className="brand"><strong>Sherman Chen</strong></a></p>
       </div>
       <div className='links-aside'>
-        <a href="/resume" className="link link-nav">Resume</a>
-        <a href="/projects" className="link link-nav">Projects</a>
-        <a href="/about" className="link link-nav">About Me</a>
-        <a href="/contact" className="link link-nav">Contact</a>
+        <p><a href="/resume" className="link link-nav">Resume</a></p>
+        <p><a href="/projects" className="link link-nav">Projects</a></p>
+        <p><a href="/about" className="link link-nav">About Me</a></p>
+        <p><a href="/contact" className="link link-nav">Contact</a></p>
       </div>
     </div>
   )
