c5e473ac35037c70e57ca3ed452ec43d65123ec5
Sherman <shc410@gmail.com>
2017-02-04 03:17:09 -0800

    resize footer

--- src/client/components/Footer.jsx
+++ src/client/components/Footer.jsx
@@ -3,7 +3,8 @@ import { h, Component } from 'preact';
 const Footer = () => {
   return (
   	<div>
-    	<p className='footer'>San Francisco Bay Area, CA, United States <a href="https://www.linkedin.com/in/shermanchen410" className="icon-link" target="_blank"><i className="fa fa-linkedin-square icon"></i></a><a href="http://github.com/Sherman-Chen" className='icon-link' target="_blank"><i className="fa fa-github icon"></i></a></p>
+    	<p className='footer'>San Francisco Bay Area, CA, United States <a href="https://www.linkedin.com/in/shermanchen410" className="icon-link" target="_blank" style={{'margin-left':'0.2rem'}}><i className="fa fa-linkedin-square icon"></i></a><a href="http://github.com/Sherman-Chen" className='icon-link' target="_blank"><i className="fa fa-github icon"></i></a>
+    	</p>
     </div>
   )
 };
