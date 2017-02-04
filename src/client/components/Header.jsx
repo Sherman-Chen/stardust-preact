fcd6a5a18f25ff99d6c86dc13e8b784d78a80116
Sherman <shc410@gmail.com>
2017-02-04 02:54:44 -0800

    adjust header, add dev script, add media query

--- src/client/components/Header.jsx
+++ src/client/components/Header.jsx
@@ -5,12 +5,12 @@ const Header = () => {
   return (
     <div className='header-flex-container'>
       <div>
-        <p><a href="/" className="brand"><strong>Sherman Chen</strong></a></p>
+        <h1><a href="/" className="brand"><strong>Sherman Chen</strong></a></h1>
       </div>
       <div className='links-aside'>
         <p><a href="/resume" className="link link-nav">Resume</a></p>
         <p><a href="/projects" className="link link-nav">Projects</a></p>
-        <p><a href="/about" className="link link-nav">About Me</a></p>
+        <p><a href="/about" className="link link-nav">About</a></p>
         <p><a href="/contact" className="link link-nav">Contact</a></p>
       </div>
     </div>
