9929d291294fbbc013bd76cbb3bfb34fde6c0865
Sherman <shc410@gmail.com>
2017-03-02 01:08:21 -0800

    focus contact

--- src/client/components/Contact.jsx
+++ src/client/components/Contact.jsx
@@ -7,15 +7,15 @@ const Contact = () => {
     <main className='flex-wrapper'>
       <Header />
       <section className='stretcher'>
-        <h1>Let's get in touch!</h1>
-        <h3><i className="fa fa-envelope icon"></i>Email</h3>
+        <h1 tabindex="1">Let's get in touch!</h1>
+        <h3 tabindex="2"><i className="fa fa-envelope icon"></i>Email</h3>
         <a className='link' href="mailto:shc410@gmail.com">shc410@gmail.com</a>
-        <h3><i className="fa fa-phone icon"></i>Phone</h3>
+        <h3 tabindex="3"><i className="fa fa-phone icon"></i>Phone</h3>
         <a className='link' href="tel:4156234670">415-623-4670</a>
         <h2>Some other stuff to look at</h2>
-        <p>Check out my <a className='link' href="https://www.linkedin.com/in/shermanchen410" target="_blank">LinkedIn</a></p>
-        <p>Check out my <a className='link' href='https://www.github.com/Sherman-Chen' target='_blank'>GitHub</a></p>
-        <p>Follow my stream of consciousness on <a className='link' href="https://twitter.com/_shermanchen" target='_blank'>Twitter</a></p>
+        <p tabindex="4">Check out my <a className='link' href="https://www.linkedin.com/in/shermanchen410" target="_blank">LinkedIn</a></p>
+        <p tabindex="5">Check out my <a className='link' href='https://www.github.com/Sherman-Chen' target='_blank'>GitHub</a></p>
+        <p tabindex="6">Follow my stream of consciousness on <a className='link' href="https://twitter.com/_shermanchen" target='_blank'>Twitter</a></p>
       </section>
       <Footer />
     </main>
